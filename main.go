a56614893f9903fe33e1afaad11d209dd7695d2b
Oleg Shalygin <oshalygin@gmail.com>
2017-12-30 01:41:20 -0800

    Add simple bot implementation

--- main.go
+++ main.go
@@ -18,4 +18,25 @@ func main() {
 	bot.Debug = true
 	log.Printf("Authorized Account: %s", bot.Self.UserName)
 
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+		message := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		message.ReplyToMessageID = update.Message.MessageID
+
+		bot.Send(message)
+
+	}
 }
