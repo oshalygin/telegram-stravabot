14a021c2a50e9614ddff4351f6340b0a18cd27ee
Oleg Shalygin <oshalygin@gmail.com>
2018-01-01 09:36:28 -0800

    Update configuration reference
    - The configuration reference file has been updated
      to use a helper function which does an env variable
      lookup with a fallback string.
    - Addition of an environment variable, literally.

--- utilities/configuration.go
+++ utilities/configuration.go
@@ -5,14 +5,21 @@ import "os"
 type Configuration struct {
 	TelegramBotAPIKey string
 	WebhookDomain     string
+	Environment       string
+}
+
+func getEnv(key string, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
 }
 
 // GetConfiguration returns back the hydrated configuration file which is based on various environment variables
 func GetConfiguration() Configuration {
-	c := Configuration{
-		TelegramBotAPIKey: os.Getenv("TELEGRAM_API_KEY"),
+	return Configuration{
+		Environment:       getEnv("ENVIRONMENT", "dev"),
+		TelegramBotAPIKey: getEnv("TELEGRAM_API_KEY", ""),
 		WebhookDomain:     "https://www.groklet.com/",
 	}
-
-	return c
 }
