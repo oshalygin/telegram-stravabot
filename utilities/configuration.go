3760970b97c79a3a997040f4ef711d320e4271bb
Oleg Shalygin <oshalygin@gmail.com>
2017-12-31 18:54:16 -0800

    Update configuration
    - The configuration file will now reference
      the webhook domain.

--- utilities/configuration.go
+++ utilities/configuration.go
@@ -4,12 +4,14 @@ import "os"
 
 type Configuration struct {
 	TelegramBotAPIKey string
+	WebhookDomain     string
 }
 
 // GetConfiguration returns back the hydrated configuration file which is based on various environment variables
 func GetConfiguration() Configuration {
 	c := Configuration{
 		TelegramBotAPIKey: os.Getenv("TELEGRAM_API_KEY"),
+		WebhookDomain:     "https://www.groklet.com/",
 	}
 
 	return c
